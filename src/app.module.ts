16fe65ef083a3c1e0ef307fa229b7811e018ece8
K070M1 <animeme_15@hotmail.com>
2025-05-13 01:05:17 -0500

    Conexion prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './db/prisma.module'
 
 @Module({
-  imports: [UsersModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [],
   providers: [],
 })
