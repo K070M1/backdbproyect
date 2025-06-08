9a1b96128f2cc89d377e531949b587d3ce237e1c
K070M1 <animeme_15@hotmail.com>
2025-06-07 19:53:16 -0500

    Changes

---
+++ src/modules/routes/routes.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+import { PrismaService } from 'src/db/prisma.service'
+
+@Injectable()
+export class RoutesService {
+  constructor(private prisma:PrismaService){}
+
+  create(createRouteDto: CreateRouteDto) {
+    return this.prisma.rutas.create({
+      data: createRouteDto
+    })
+  }
+
+  findAll() {
+    return `This action returns all routes`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} route`;
+  }
+
+  update(id: number, updateRouteDto: UpdateRouteDto) {
+    return `This action updates a #${id} route`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} route`;
+  }
+}
